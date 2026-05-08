2f7beeb8ef3f0ae12bd094335afc34d0949209e7
Gabriel Arcanjo <gabrielarcanjo605@gmail.com>
2026-05-08 16:14:41 -0300

    Add SQL query to retrieve active customers and plans

---
+++ scripts/02_clientes_ativos.sql
@@ -0,0 +1,8 @@
+SELECT C.company_name, C.industry, P.plan_name, S.Status
+FROM subscriptions S
+INNER JOIN plans P
+	ON  P.plan_id = S.plan_id
+INNER JOIN Customers C
+		ON C.customer_id = S.customer_id
+WHERE S.status = 'active'
+ORDER BY C.company_name
